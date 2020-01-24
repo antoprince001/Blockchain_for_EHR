d220bef4c9884972f88ebe1bd9eff6026ebc9fe3
Antony Prince <47826916+CodeDecoder01@users.noreply.github.com>
2020-01-24 22:53:05 +0530

    Update smart.py

--- smart.py
+++ smart.py
@@ -11,7 +11,7 @@ import pymongo,dns
 }'''
 
 
-client = pymongo.MongoClient("mongodb+srv://Antony:A8939469555@blockchainehr-kpbxk.mongodb.net/test?retryWrites=true&w=majority")
+client = pymongo.MongoClient()
 db = client.test
 mydb=client["Blockchain"]
 
